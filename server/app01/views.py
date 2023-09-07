ee1b6bcc9fc950c39ef5f3c27ebac2d46ece91e2
Critobal Navarrete <cristobalnavarrete2019@gmail.com>
2023-09-07 10:02:09 -0300

    rama 1 vista

--- server/app01/views.py
+++ server/app01/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def vista1(request):
+ return  HttpResponse("<h1> hola 1 </h1>")
--- server/server/urls.py
+++ server/server/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app01 import views as v1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('v1/',v1.vista1)
 ]
